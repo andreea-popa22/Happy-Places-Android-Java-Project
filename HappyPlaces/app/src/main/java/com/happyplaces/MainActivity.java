fc28be528cdde04fbb9a6e1d82fc851e7b4f07ef
Andreea <andreea_popa22@yahoo.ro>
2022-05-15 13:28:34 +0300

    make action bar gradient

--- HappyPlaces/app/src/main/java/com/happyplaces/MainActivity.java
+++ HappyPlaces/app/src/main/java/com/happyplaces/MainActivity.java
@@ -2,7 +2,12 @@ package com.happyplaces;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
