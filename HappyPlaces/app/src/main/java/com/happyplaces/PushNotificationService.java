46bb95ab386b821a1059a68faefe0cb78d8f794a
Gabi Badescu <gabi_badescu99@yahoo.com>
2022-05-17 22:35:59 +0300

    notificiation service

---
+++ HappyPlaces/app/src/main/java/com/happyplaces/PushNotificationService.java
@@ -0,0 +1,11 @@
+package com.happyplaces;
+
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class PushNotificationService extends FirebaseMessagingService {
+
+}
